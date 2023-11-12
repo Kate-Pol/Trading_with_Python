3c0e7f817b1eed6993ac869eca0630ef4bbece04
Kate Polishchuk <katepolishchuk13@gmail.com>
2023-11-12 16:18:41 -0500

    updated main

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
 
+# Load data
+
+company = 'FB'
+
+start = dt.datetime(2012,1,1)
+end = dt.datetime(2023,11,11)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+#Prepare data 
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[])
+
+
+
