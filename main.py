1fb10e1c9a43921b79a6947e8fcb47a72bc4aff9
Kate Polishchuk <katepolishchuk13@gmail.com>
2023-11-14 21:06:21 -0500

    added model data

--- main.py
+++ main.py
@@ -29,7 +29,34 @@ x_train = []
 y_train = []
 
 for x in range(prediction_days, len(scaled_data)):
-    x_train.append(scaled_data[])
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+    
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train[1], 1))
+
+# Build the Model
+model = Sequential()
+
+model.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1)) # Prediction of the next closing
+
+model.compile(optimazer='adam', loss='mean_squared_error')
+model.fir(x_train, y_train, epochs=25, batch_size=32)
+
+''' TEST THE MODEL ACCURACY ON EXISTING DATA '''
+
+#Load test data
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
 
 
 
